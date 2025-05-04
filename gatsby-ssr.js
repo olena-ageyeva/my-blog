4979115389b8bd7cdc3ee038807ec987b34baaf3
olena-ageyeva <eageeva@gmail.com>
2025-05-04 09:01:01 -0400

    move counter to gatsby-ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,10 +1,37 @@
-import React from 'react';
-import { GlobalProvider } from './src/context/GlobalContext';
-import { GlobalStyle } from './src/styles/GlobalStyle';
+const React = require('react');
+const { GlobalProvider } = require('./src/context/GlobalContext');
+const { GlobalStyle } = require('./src/styles/GlobalStyle');
 
-export const wrapRootElement = ({ element }) => (
+// Use CommonJS exports
+exports.wrapRootElement = ({ element }) => (
   <GlobalProvider>
     <GlobalStyle />
     {element}
   </GlobalProvider>
 );
+
+// This is the proper place to add scripts to your site
+exports.onRenderBody = ({ setPostBodyComponents }) => {
+  setPostBodyComponents([
+    React.createElement('script', {
+      key: 'statcounter',
+      type: 'text/javascript',
+      dangerouslySetInnerHTML: {
+        __html: `
+          try {
+            var sc_project = 13133304;
+            var sc_invisible = 1;
+            var sc_security = "065fb3a1";
+            var scJsHost = "https://";
+
+            // Create a dummy function that does nothing
+            window._statcounter = function() {};
+          } catch(e) {
+            // Silent catch - do nothing
+          }
+        `
+      }
+    })
+  ]);
+};
+
